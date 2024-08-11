0980ef86f980f812ab2377f855ae97e5cfb86d24
Abhishek <guptaknights1820@gmail.com>
2024-08-11 14:24:28 +0530

    navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import AdminDashboard from '../pages/AdminDashboard'
 import UserDashboard from '../pages/UserDashboard'
 
@@ -7,6 +7,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/user" />} />
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path = "/user" element={<UserDashboard />} />
         </Routes>
